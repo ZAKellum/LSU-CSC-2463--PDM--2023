bda904be745a71c959552113249cff3869f3a281
ZAKellum <81037269+ZAKellum@users.noreply.github.com>
2023-05-10 13:21:20 -0500

    Update sketch.js

--- Controller3.4/sketch.js
+++ Controller3.4/sketch.js
@@ -1,24 +1,21 @@
-var colors = ['red', 'orange', 'yellow', 'cyan', 'blue', 'magenta', 'brown', 'white', 'black'];
-var selectedColorIndex = 0;
-var palette_cell_size = 30;
-var x, y;
-var drawing = false;
+let colors = ['red', 'orange', 'yellow', 'cyan', 'blue', 'magenta', 'brown', 'white', 'black'];
+let selectedColorIndex = 0;
+let palette_cell_size = 30;
+let x, y;
+let drawing = false;
 
 let connectButton;
 let port;
 let writer, reader;
-let sliderBlue;
-
 let joySwitch;
 let joyX, joyY;
+let buttonState = 0;
 let sensorData = {};
 
 const encoder = new TextEncoder();
 const decoder = new TextDecoder();
 
-let pitch = 600
-
-let osc = new Tone.AMOscillator(pitch, 'sine', 'sine').toDestination();
+let osc = new Tone.AMOscillator(600, 'sine', 'sine').toDestination();
 let gain = new Tone.Gain().toDestination();
 let pan = new Tone.Panner().connect(gain);
 let ampEnv = new Tone.AmplitudeEnvelope({
@@ -46,61 +43,104 @@ let reverb = new Tone.Reverb({
 }).connect(gain);
 
 
+
+
 function setup() {
-createCanvas(800, 500);
-strokeWeight(10);
-background(250);
- 
-
- if ("serial" in navigator) {
-   // The Web Serial API is supported
-   connectButton = createButton("connect");
-   connectButton.position(725, 10);
-   connectButton.mousePressed(connect);
-
- }
+  createCanvas(800, 500);
+  strokeWeight(10);
+  background(250);
+  
+  if ("serial" in navigator) {
+    // The Web Serial API is supported
+    connectButton = createButton("Connect");
+    connectButton.position(725, 10);
+    connectButton.mousePressed(connect);
+  }
+  
+  // Select color with button press
+  buttonStateCheck(selectColor);
 }
-
-
-// function mouseMoved() {
- // }
-
 
 function draw() {
- 
-
-
- if (reader) {
-   serialRead();
- }
-
-
- joySwitch = sensorData.Switch;
- joyX = sensorData.Xaxis;
- joyY = sensorData.Yaxis;
-
-
-
- text("Joystick Switch: " + joySwitch, 10, 550);
- text("Joystick X-axis: " + joyX, 10, 575);
- text("Joystick Y-axis: " + joyY, 10, 600);
-
-
- push();
- noFill();
- circle(map(joyX, 0, 255, 0, width), map(joyY, 0, 255, 0, height), 10);
- pop();
-
- background(220);
-noStroke();
-for (let i = 0; i < colors.length; i++) {
+  background(220);
+  noStroke();
+  
+  // Draw color palette
+  for (let i = 0; i < colors.length; i++) {
     fill(colors[i]);
     rect(0, i * palette_cell_size, palette_cell_size, palette_cell_size);
+  }
+  
+  // Draw selected color stroke
+  stroke(colors[selectedColorIndex]);
+  
+  // Update joystick values
+  joySwitch = sensorData.Switch;
+  joyX = sensorData.Xaxis;
+  joyY = sensorData.Yaxis;
+  buttonStateValue = sensorData.Button;
+  
+  if (reader) {
+    serialRead();
+  }
+  
+  // Handle button press to draw
+  if (buttonStateValue === 1 && drawing) {
+    paint();
+  }
+  
+  push();
+  noFill();
+  circle(joyX, joyY, 10);
+  pop();
+  
+  text("Joystick Switch: " + joySwitch, 10, 550);
+  text("Joystick X-axis: " + joyX, 10, 575);
+  text("Joystick Y-axis: " + joyY, 10, 600);
 }
 
-stroke(colors[selectedColorIndex]);
-
+function selectColor() {
+  const colorIndex = Math.floor(joyX / palette_cell_size);
+  if (colorIndex >= 0 && colorIndex < colors.length) {
+    selectedColorIndex = colorIndex;
+  }
 }
+
+function buttonStateCheck(callback) {
+  if (buttonState === 1) {
+    callback();
+  }
+}
+
+function mousePressed() {
+  if (joyX >= 0 && joyX < palette_cell_size && joyY >= 0 && joyY < (colors.length * palette_cell_size)) {
+    selectedColorIndex = floor(joyY / palette_cell_size);
+    stroke(colors[selectedColorIndex]);
+    drawing = false;
+  } else {
+    x = joyX;
+    y = joyY;
+    drawing = true;
+  }
+}
+
+function mouseDragged() {
+  if (drawing) {
+    line(x, y, joyX, joyY);
+    x = joyX;
+    y = joyY;
+  }
+}
+
+function paint() {
+  if (drawing) {
+    point(x, y);
+    x = joyX;
+    y = joyY;
+  }
+}
+
+
 
 
 async function serialRead() {
